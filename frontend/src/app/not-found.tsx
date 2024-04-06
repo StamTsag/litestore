adbcd2dd093194eb8cc6b637a8bb21a26e5545f4
Shadofer <shadowrlrs@gmail.com>
2024-04-06 23:57:03 +0300

    Minor bug fix

--- frontend/src/app/not-found.tsx
+++ frontend/src/app/not-found.tsx
@@ -1,18 +1,12 @@
 import BlurredBackground from "./components/global/Background";
 import LitestoreContentNotFound from "./components/global/NotFound";
-import LitestoreBar from "./components/global/TopBar";
-import LitestoreFooter from "./components/global/Footer";
 
 export default function Page() {
   return (
     <div>
       <BlurredBackground />
 
-      <LitestoreBar />
-
       <LitestoreContentNotFound />
-
-      <LitestoreFooter />
     </div>
   );
 }
